24d65a5a645256664522ed2d42d853f5bc810297
Monish Gupta <monish.gupta1@gmail.com>
2016-01-27 19:16:53 +0530

    Added code for lesson 6 - regression.

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,9 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
+
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +31,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,13 +39,26 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+target_test_pred = reg.predict(feature_test)
+print "coef ", reg.coef_
+print "intercept: ", reg.intercept_
+print "score on training data: ", reg.score(feature_train, target_train)
+print "score on test data: ", reg.score(feature_test, target_test, sample_weight=None) 
 
+##bonus, long_term_incentive
+#coef  [ 1.19214699]
+#intercept:  554478.756215
+#score on training data:  0.217085971258
+#score on test data:  -0.59271289995
 
-
-
-
-
-
+##bonus, salary
+#coef  [ 5.44814029]
+#intercept:  -102360.543294
+#score on training data:  0.0455091926995
+#score on test data:  -1.48499241737
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -64,6 +79,9 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+print  "New slope:", reg.coef_
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
