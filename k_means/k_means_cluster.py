ed3bb6140b6f63bc4997edd047d91fa8b34f6540
Monish Gupta <Monish Gupta>
2016-02-13 10:31:01 +0530

    KMeans clustering with 2 features and target as 'POI'

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,7 +48,7 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
+poi = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
@@ -64,13 +64,16 @@ plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
-
+from sklearn import cluster
+n_clusters=2
+clf = cluster.KMeans(n_clusters=n_clusters)
+print "clf: ", clf
+clf.fit(data)
+pred = clf.predict(data)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters2.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
